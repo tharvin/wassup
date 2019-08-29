aa211449d482bb3585660f6f17eab32af84fa14e
tharvin <54672733+tharvin@users.noreply.github.com>
2019-08-29 08:39:13 -0500

    Adding code to print out name

---
+++ hello.c
@@ -0,0 +1,14 @@
+/* 
+Auhtor; Tharvin 
+Date; 2019/08/29
+*
+A simple hello world program
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	printf("Tharvin\n");
+	return 0;
+	}
+	
